d8f30e2249f84bbae17cdc6e03d7f4c92b7f955c
Bapugouda <rajjdanush@gmail.com>
2023-09-04 16:36:53 +0530

    Initial Typescript code Execution

---
+++ fetchjson/index.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+//interface used to define structure of object properties, help us to catch development errors
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+  The Todo with id: ${id}
+  Has a title of : ${title}
+  Is it finished? ${completed}
+  `);
+});
+
